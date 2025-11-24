8c86867ef9468cc0d849bb5de99c2a524b679e07
sreehariperozhi <sreehariperozhi07@gmail.com>
2025-11-24 22:55:13 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BROWSER-BASED-PDF-TOOLKIT/sw.js', { scope: '/BROWSER-BASED-PDF-TOOLKIT/' })})}
