4a0301a7225142144d2bc24fd1319de0a2ea5df5
sreehariperozhi <sreehariperozhi07@gmail.com>
2025-11-28 17:39:53 +0530

    Updates

--- assets/jszip.min-D1o_p4I2.js
+++ assets/jszip.min-D1o_p4I2.js
@@ -1,4 +1,4 @@
-import{a as Ot,c as vt}from"./pdf-lib-CCG-A_qJ.js";import{c as yt}from"./pdfjs-B00Zt0VO.js";function Bt(ct,wt){for(var _=0;_<wt.length;_++){const N=wt[_];if(typeof N!="string"&&!Array.isArray(N)){for(const w in N)if(w!=="default"&&!(w in ct)){const h=Object.getOwnPropertyDescriptor(N,w);h&&Object.defineProperty(ct,w,h.get?h:{enumerable:!0,get:()=>N[w]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var zt={exports:{}};/*!
+import{a as Ot,c as vt}from"./pdf-lib-EXFfEM8H.js";import{c as yt}from"./pdfjs-BgLFfVRi.js";function Bt(ct,wt){for(var _=0;_<wt.length;_++){const N=wt[_];if(typeof N!="string"&&!Array.isArray(N)){for(const w in N)if(w!=="default"&&!(w in ct)){const h=Object.getOwnPropertyDescriptor(N,w);h&&Object.defineProperty(ct,w,h.get?h:{enumerable:!0,get:()=>N[w]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var zt={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BROWSER-BASED-PDF-TOOLKIT/sw.js', { scope: '/BROWSER-BASED-PDF-TOOLKIT/' })})}
